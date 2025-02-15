dc5093710d67c3805bb7395a40be8e27aaf092ed
Nguyen Da Da <nguyendazero@gmail.com>
2025-02-15 20:30:34 +0700

    fill posts data in index.html home page

--- haibazo-bff-its-rct-webapi/src/main/java/com/haibazo_bff_its_rct_webapi/controller/HomeController.java
+++ haibazo-bff-its-rct-webapi/src/main/java/com/haibazo_bff_its_rct_webapi/controller/HomeController.java
@@ -1,14 +1,8 @@
 package com.haibazo_bff_its_rct_webapi.controller;
 
 import com.haibazo_bff_its_rct_webapi.dto.APICustomize;
-import com.haibazo_bff_its_rct_webapi.dto.response.ItsRctCategoryResponse;
-import com.haibazo_bff_its_rct_webapi.dto.response.ItsRctCollectionResponse;
-import com.haibazo_bff_its_rct_webapi.dto.response.ItsRctProductResponse;
-import com.haibazo_bff_its_rct_webapi.dto.response.ItsRctStyleResponse;
-import com.haibazo_bff_its_rct_webapi.service.CategoryService;
-import com.haibazo_bff_its_rct_webapi.service.CollectionService;
-import com.haibazo_bff_its_rct_webapi.service.ProductService;
-import com.haibazo_bff_its_rct_webapi.service.StyleService;
+import com.haibazo_bff_its_rct_webapi.dto.response.*;
+import com.haibazo_bff_its_rct_webapi.service.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -26,27 +20,21 @@ import java.util.List;
 public class HomeController {
 
     private final ProductService productService;
-    private final CategoryService categoryService;
-    private final StyleService styleService;
-    private final CollectionService collectionService;
+    private final PostService postService;
 
     @GetMapping("/public/home")
     public String home(Model model) {
 
-        APICustomize<List<ItsRctCategoryResponse>> categoryResponse = categoryService.categories();
-        APICustomize<List<ItsRctStyleResponse>> stylesResponse = styleService.styles();
-        APICustomize<List<ItsRctCollectionResponse>> collectionResponse = collectionService.collections();
+        APICustomize<List<ItsRctPostResponse>> postResponse = postService.posts();
         APICustomize<List<ItsRctProductResponse>> productDiscountResponse = productService.getDiscountedProducts();
         APICustomize<List<ItsRctProductResponse>> productResponse = productService.products(
                 null, null, null, null, null, null, null, null, null, null,  null, 0, 10);
-        
+
+        model.addAttribute("posts", postResponse.getResult());
         model.addAttribute("products", productResponse.getResult());
         model.addAttribute("productsDiscount", productDiscountResponse.getResult());
         model.addAttribute("multiplier", BigDecimal.valueOf(2));
-        model.addAttribute("categories", categoryResponse.getResult());
-        model.addAttribute("styles", stylesResponse.getResult());
-        model.addAttribute("collections", collectionResponse.getResult());
-
+        
         return "index";
     }
     
